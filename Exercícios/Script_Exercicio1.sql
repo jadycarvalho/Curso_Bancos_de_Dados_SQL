197383d99526383031e11df9e32791c0602d8cbc
Jady Carvalho <79340031+jadycarvalho@users.noreply.github.com>
2021-03-04 12:31:04 -0300

    Add files via upload

--- Exercícios/Script_Exercicio1.sql
+++ Exercícios/Script_Exercicio1.sql
@@ -22,6 +22,8 @@ STATUS + ENTER
 /* CONEVCTANDO A UM BANCO */
 USE LIVRARIA;
 
+
+
 INSERT INTO LIVROS VALUES('Cavaleiro Real','Ana Claudia','F',465,'Atlas',49.9,'RJ',2009);
 INSERT INTO LIVROS VALUES('SQL para leigos','João Nunes','M',450,'Addison',98,'SP',2018);
 INSERT INTO LIVROS VALUES('Receitas Caseiras','Celia Tavares','F',210,'Atlas',45,'RJ',2008);
@@ -36,22 +38,69 @@ INSERT INTO LIVROS VALUES('O poder da mente','Clara Mafra','F',120,'Continental'
 /*1 – Trazer todos os dados.*/
 select * from livros;
 
++---------------------+-------------------+------+---------+-------------+--------+------+------+
+| LIVRO               | AUTOR             | SEXO | PAGINAS | EDITORA     | VALOR  | UF   | ANO  |
++---------------------+-------------------+------+---------+-------------+--------+------+------+
+| Cavaleiro Real      | Ana Claudia       | F    |     465 | Atlas       |  49.90 | RJ   | 2009 |
+| SQL para leigos     | João Nunes        | M    |     450 | Addison     |  98.00 | SP   | 2018 |
+| Receitas Caseiras   | Celia Tavares     | F    |     210 | Atlas       |  45.00 | RJ   | 2008 |
+| Pessoas Efetivas    | Eduardo Santos    | M    |     390 | Beta        |  78.00 | RJ   | 2018 |
+| Habitos Saudáveis   | Eduardo Santos    | M    |     630 | Beta        | 150.00 | RJ   | 2019 |
+| A Casa Marrom       | Hermes Macedo     | M    |     250 | Bubba       |  60.00 | MG   | 2016 |
+| Estacio Querido     | Geraldo Francisco | M    |     310 | Insignia    | 100.00 | ES   | 2015 |
+| Pra sempre amigas   | Leda Silva        | F    |     510 | Insignia    |  78.00 | ES   | 2011 |
+| Copas Inesqueciveis | Marco Alcantara   | M    |     200 | Larson      | 130.00 | RS   | 2018 |
+| O poder da mente    | Clara Mafra       | F    |     120 | Continental |  56.00 | SP   | 2017 |
++---------------------+-------------------+------+---------+-------------+--------+------+------+
+
 /*2 – Trazer o nome do livro e o nome da editora*/
 SELECT LIVRO, EDITORA
 FROM LIVROS;
+
++---------------------+-------------+
+| LIVRO               | EDITORA     |
++---------------------+-------------+
+| Cavaleiro Real      | Atlas       |
+| SQL para leigos     | Addison     |
+| Receitas Caseiras   | Atlas       |
+| Pessoas Efetivas    | Beta        |
+| Habitos Saudáveis   | Beta        |
+| A Casa Marrom       | Bubba       |
+| Estacio Querido     | Insignia    |
+| Pra sempre amigas   | Insignia    |
+| Copas Inesqueciveis | Larson      |
+| O poder da mente    | Continental |
++---------------------+-------------+
 
 /*3 – Trazer o nome do livro e a UF dos livros publicados por autores do sexo masculino.*/
 SELECT LIVRO, UF
 FROM LIVROS
 WHERE SEXO = 'M';
 
++---------------------+------+
+| LIVRO               | UF   |
++---------------------+------+
+| SQL para leigos     | SP   |
+| Pessoas Efetivas    | RJ   |
+| Habitos Saudáveis   | RJ   |
+| A Casa Marrom       | MG   |
+| Estacio Querido     | ES   |
+| Copas Inesqueciveis | RS   |
++---------------------+------+
 
 /*4 - Trazer o nome do livro e o número de páginas dos livros publicados por autores do sexo feminino.*/
-
 SELECT LIVRO, PAGINAS
 FROM LIVROS
 WHERE SEXO = 'F';
 
++-------------------+---------+
+| LIVRO             | PAGINAS |
++-------------------+---------+
+| Cavaleiro Real    |     465 |
+| Receitas Caseiras |     210 |
+| Pra sempre amigas |     510 |
+| O poder da mente  |     120 |
++-------------------+---------+
 
 /*5 – Trazer os valores dos livros das editoras de São Paulo.*/
 
@@ -59,24 +108,49 @@ SELECT LIVRO, VALOR
 FROM LIVROS
 WHERE UF = 'SP';
 
++------------------+-------+
+| LIVRO            | VALOR |
++------------------+-------+
+| SQL para leigos  | 98.00 |
+| O poder da mente | 56.00 |
++------------------+-------+
 
 /*6 – Trazer os dados dos autores do sexo masculino que tiveram livros publicados por São Paulo ou Rio de Janeiro (Questão Desafio).*/
-
 SELECT AUTOR, SEXO, UF
 FROM LIVROS
 WHERE SEXO = 'M'
 AND UF = 'SP'
-OR UF = 'RJ'; /* AQUI VAI ENTRAR DO SEXO FEMININO TAMBÉM */
+OR UF = 'RJ'; 
++----------------+------+------+
+| AUTOR          | SEXO | UF   |
++----------------+------+------+
+| Ana Claudia    | F    | RJ   |
+| João Nunes     | M    | SP   |
+| Celia Tavares  | F    | RJ   |
+| Eduardo Santos | M    | RJ   |
+| Eduardo Santos | M    | RJ   |
++----------------+------+------+
 
 SELECT AUTOR, SEXO, UF
 FROM LIVROS
 WHERE 
 SEXO = 'M' AND (UF = 'SP' OR UF = 'RJ');
++----------------+------+------+
+| AUTOR          | SEXO | UF   |
++----------------+------+------+
+| João Nunes     | M    | SP   |
+| Eduardo Santos | M    | RJ   |
+| Eduardo Santos | M    | RJ   |
++----------------+------+------+
 
 /*7 – Trazer os dados dos autores do sexo masculino que tiveram livros publicados por São Paulo ou Rio de Janeiro e páginas maior que 100.*/
-
 SELECT AUTOR, SEXO, UF
 FROM LIVROS
 WHERE SEXO = 'M'
 AND UF = 'SP'
 AND paginas > 100;
++-------------+------+------+
+| AUTOR       | SEXO | UF   |
++-------------+------+------+
+| João Nunes  | M    | SP   |
++-------------+------+------+
